2c74c5bc25e74acdadbea90e5edc7f9d4fa60dcf
akproscia <annakproscia@gmail.com>
2026-03-18 15:56:34 -0400

    started profile class

---
+++ js/profile.js
@@ -0,0 +1,30 @@
+class user {
+
+    constructor(displayName, userName, email, password){
+        this.displayName = displayName;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    setDisplayName(displayName) {
+        this.displayName = displayName;
+    }
+
+    setUserName(userName) {
+        this.userName = userName;
+    }
+
+    setBio
+
+    getDisplayName() {
+        return this.displayName;
+    }
+
+    getUserName() {
+        return this.userName;
+    }
+
+
+
+}
