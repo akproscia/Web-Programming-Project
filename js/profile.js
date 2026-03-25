79ed3555d4a98e324b32893eae1714e0ab7651aa
akproscia <annakproscia@gmail.com>
2026-03-25 15:24:54 -0400

    in-class edits

--- js/profile.js
+++ js/profile.js
@@ -1,23 +1,33 @@
-class user {
+class User {
 
-    constructor(displayName, userName, email, password){
+    constructor(displayName, userName, email, password, bio, interests){
         this.displayName = displayName;
         this.userName = userName;
         this.email = email;
         this.password = password;
         
+        this.bio = bio;
+        this.interests = []; //function to split from list by comments
+        this.following = []; //should we have followers and following instead?
+
     }
 
+    // Setters
     setDisplayName(displayName) {
         this.displayName = displayName;
     }
     setUserName(userName) {
         this.userName = userName;
     }
+    setPassword(password) {
+        this.password = password;
+    }
     setBio(bio) {
         this.bio = bio;
     }
 
+
+    // Getters
     getDisplayName() {
         return this.displayName;
     }
@@ -28,5 +38,27 @@ class user {
         return this.bio;
     }
 
+    addInterest(interest) {
+        this.interests.push(interest);
+    }
+    //adds to the list of users they are following
+    addFollowing(user){
+        this
+    }
 
+}
+
+class App {
+    // read inputs and create an account
+    constructor() {
+        // set up references
+        this.displayName = document.querySelector("#");
+
+
+    }
+
+    createAccount() {
+        const account = new User();
+        this.users.push(account);
+    }
 }
