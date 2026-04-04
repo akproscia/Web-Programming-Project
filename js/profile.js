5a33a4d55fd8a526e86ccb305643435b5c1d235e
akproscia <annakproscia@gmail.com>
2026-04-03 20:37:38 -0400

    Added to profile.js.

--- js/profile.js
+++ js/profile.js
@@ -1,51 +1,54 @@
 class User {
 
-    constructor(displayName, userName, email, password, bio, interests){
+    constructor(displayName, userName, email, password, bio){
         this.displayName = displayName;
         this.userName = userName;
         this.email = email;
         this.password = password;
-        
         this.bio = bio;
+
         this.interests = []; //function to split from list by comments
         this.following = []; //should we have followers and following instead?
+        this.friends = []; //mutuals
 
+        this.contacts = {
+            spotify: null,
+            letterboxd: null,
+            goodreads: null,
+            instagram: null,
+            discord: null,
+            steam: null,
+        };
     }
 
     // Setters
-    setDisplayName(displayName) {
-        this.displayName = displayName;
-    }
-    setUserName(userName) {
-        this.userName = userName;
-    }
-    setPassword(password) {
-        this.password = password;
-    }
-    setBio(bio) {
-        this.bio = bio;
-    }
+    setDisplayName(displayName) { this.displayName = displayName; }
+    setUserName(userName) { this.userName = userName; }
+    setPassword(password) { this.password = password; }
+    setBio(bio) { this.bio = bio; }
 
 
     // Getters
-    getDisplayName() {
-        return this.displayName;
+    getDisplayName() { return this.displayName; }
+    getUserName() { return this.userName; }
+    getBio() { return this.bio; }
+
+    setContact(platform, url) {
+        // need to chech to make sure that the platform exists and is in our system
+        this.contacts[platform] = url;
     }
-    getUserName() {
-        return this.userName;
+    //adds to the list of users they are following
+    addFollowing(user){
+        if (!this.following.includes(user)) {
+            this.following.push(user);
+        }
     }
-    getBio() {
-        return this.bio;
-    }
+
 
     addInterest(interest) {
         this.interests.push(interest);
     }
-    //adds to the list of users they are following
-    addFollowing(user){
-        this
-    }
-
+    
 }
 
 class App {
@@ -56,6 +59,36 @@ class App {
 
 
     }
+
+    /*
+    Needs to be developed further!
+    showContacts(user) {
+        const container = document.querySelector(".connect-list");
+
+        const icons = {
+            spotify: "/images/spotify-logo.png",
+            letterboxd: "/images/letterboxd-logo.png",
+            goodreads: "/images/goodreads-logo.png",
+            instagram: "/images/instagram-logo.png",
+            discord: "/images/discord-logo.png",
+            steam: "/images/steam-logo.png",
+        };
+
+        for (const [platform, url] of this.user.contacts) {
+            if (url != null) {
+                const item = document.createElement("div");
+                item.classList.add("connect-item"); // make connect-item class in css!
+                const img = document.createElement("img");
+                img.src = icons[platform];
+                const link = document.createElement("a");
+                link.href = url;
+                link.
+                container.appendChild();
+
+            }
+        }
+    }
+    */
 
     createAccount() {
         const account = new User();
