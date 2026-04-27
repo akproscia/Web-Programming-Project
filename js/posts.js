d495d9544a2b58dd64be9917e79ea2461740db33
akproscia <annakproscia@gmail.com>
2026-04-27 13:40:41 -0400

    Implemented JS functionality to index.html, ability to create and load posts using App class from posts.js.

--- js/posts.js
+++ js/posts.js
@@ -12,40 +12,44 @@ class SocialMediaPost {
         this.div = document.createElement("div");
         this.div.classList.add("post");
 
-        // // create the profile pic image and add it to the div instance variable
-        // const image = document.createElement("img");
-        // image.classList.add("profile-pic");
-        // image.src = this.profilePicURL;
-        // this.div.appendChild(image);
-
         // create the post title and add it to the div
         const title = document.createElement("h3");
         title.classList.add("post-title");
         title.textContent = postTitle;
+        this.div.appendChild(title);
 
-        const username = document.createElement("p i");
+        // create the media badge and add it to the div
+        const badge = document.createElement("p");
+        badge.classList.add("media-badge");
+        badge.textContent = mediaType;
+        this.div.appendChild(badge);
+
+        // create username element and add it to the div
+        const username = document.createElement("p");
         username.classList.add("post-username");
         username.textContent = `Posted by ${this.user}`;
-
-        this.div.appendChild(title);
-        this.div.appendChild(username);
-
-        // media type
-        
+        this.div.appendChild(username);        
 
         // create the post text paragraph and add it to the div instance variable
         const p = document.createElement('p');
         p.textContent = postText;
         this.div.appendChild(p);
 
-        // add the div to the parent element
-        this.parentElement.appendChild(this.div);
+        // add the div to the DOM
+        this.addToDOM();
+    }
 
+    remove() {
+        this.div.remove();
+    }
+
+    addToDOM() {
+        this.parentElement.appendChild(this.div);
     }
 }
 
 class App {
-    constructor(){
+    constructor() {
         // App should handle references to the post container, text input, other passed values, etc.
         // It should also havr all methods that deal with creating and editing posts. everything will be done through this class. 
         
@@ -55,17 +59,69 @@ class App {
         // It is intended to serve as a starting point, and we can adjust it as needed going forward.
 
         // set up a reference to the post container in the html
-        this.postContainer = document.querySelector("#post-container");
+        this.postContainer = document.getElementById("post-container");
         // set up a reference to the text input
-        this.textInput = document.querySelector('#post-body');
+        this.textInput = document.getElementById("post-body");
         // set up an array variable that will hold the posts 
         this.posts = [];
 
-        this.addPost = this.addPost.bind(this);
-        this.sortPosts = this.sortPosts.bind(this);
+        this.submitPost = this.submitPost.bind(this);
+        this.refreshPosts = this.refreshPosts.bind(this);
 
-        document.querySelector("#add-post").addEventListener('submit', this.addPost);
-        document.querySelector("#sort").addEventListener("click", this.sortPosts);
+        document.getElementById("add-post").addEventListener('submit', this.submitPost);
+        document.getElementById("refresh-posts").addEventListener("click", this.refreshPosts);
+    }
+
+    async loadPosts() {
+        // fetch the posts from data/posts.json and save them to an array variable in the App object
+        const response = await fetch("data/posts.json");
+        const data = await response.json();
+        
+        // create a new post for each object in the data array
+        for (const obj of data) {
+            this.createPost(obj);
+        }
+
+        // enable the post form and give it a submit listener        
+        this.postForm = document.getElementById("add-post");
+        this.postForm.addEventListener('submit', this.submitPost);
+
+        // enable the sort button and give it a click listener
+        this.refreshButton = document.getElementById("refresh-posts"); 
+        this.refreshButton.addEventListener("click", this.refreshPosts);
+    }
+
+    createPost(obj) {
+        // obj should have postTitle, postText, mediaType, and user
+        const post = new SocialMediaPost(this.postContainer, obj.postTitle, obj.postText, obj.mediaType, obj.user);
+        this.posts.push(post);
+    }
+    
+    submitPost(event) {
+        event.preventDefault(); // prevent the form from refreshing the page
+
+        const obj = {
+            postTitle: document.getElementById("post-title").value,
+            postText: document.getElementById("post-body").value,
+            mediaType: document.getElementById("media-type").value,
+            user: { userName: document.getElementById("user-name").value } // add pfp later?
+        }
+
+        this.createPost(obj);
+
+        // clear the form inputs
+        document.getElementById("post-title").value = "";
+        document.getElementById("post-body").value = "";
+        document.getElementById("media-type").value = "";
+        document.getElementById("user-name").value = "";
+    }
+
+    refreshPosts() {
+        // remove all of the posts from the DOM and then add them back in the order they are in the this.posts array
+        for (const post of this.posts) {
+            post.remove();
+            post.addToDOM();
+        }
     }
 }
 
