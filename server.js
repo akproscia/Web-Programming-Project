b764841fcf8881de6f85541bde35877f7c17eee9
akproscia <annakproscia@gmail.com>
2026-05-05 14:16:39 -0400

    added folder organization and updated server.js to handle writing posts to JSON files.

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 import express from 'express';
 import fs from 'fs';
+
+// Import posts and user data
 import initialUsers from './data/users.json' with { type: 'json' };
+import posts from './data/posts.json' with { type: 'json' };
+
+const app = express();
+
+app.use(express.json()); //allows us to parse through JSON files
+app.use(express.static('public')); // Serves static files from the public folder
+
+
+/********************
+** User Management **
+********************/
 
 class UserManager {
     constructor(dataPath, users) {
@@ -50,12 +63,7 @@ class UserManager {
     }
 }
 
-const app = express();
 const userManager = new UserManager('./data/users.json', initialUsers);
-
-app.use(express.json());
-// Serves static files from the root and specific folders
-app.use(express.static('./')); 
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
@@ -65,6 +73,37 @@ app.post('/login', (req, res) => {
 app.post('/register', (req, res) => {
     res.json(userManager.registerUser(req.body));
 });
+
+
+/********************
+** Post Management **
+********************/
+
+app.get('/data', (req, res) => {
+    res.json(posts);
+});
+
+app.post('/save-data', (req, res) => {
+    posts.push(req.body); // add the new post to the posts array in memory
+    console.log(posts); // log the posts array to the console to verify that the new post was added
+    fs.writeFile(
+        'data/posts.json',
+        JSON.stringify(posts),
+        'utf8',
+        (err) => {
+            if (err) console.log(err);
+            else console.log('Data Written Successfully!');
+        }
+    );
+});
+
+app.get('/failed-to-login', (req, res) => {
+    res.sendFile('public/failed-to-login.html', { root: __dirname });
+});
+
+/*****************
+** Start Server **
+*****************/
 
 app.listen(3000, () => {
     console.log('Server running at http://localhost:3000');
